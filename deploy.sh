919a12c8a43e020c32426d9ac9d95137963d9ede
Yoni Bettan <ybettan@redhat.com>
2020-04-22 16:07:26 +0300

    Added mTLS - NOT WORKING YET
    There is a bug in observatroiu/observatorium which make /-/healthy and
    /-/ready endpointes require client-certificate as well as read/write
    endpoints therefore k8s fails to deploy the pod.
    
    I have changed `livenessProbe` and `healthnessProbe` to use TCP instead
    of HTTP in the mean while and the pod is deployed successfully.
    
    Metrics cannot be delivered to Observatorium because of a
    prometheus-operator issue
    https://github.com/coreos/prometheus-operator/issues/3118.
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,21 @@
 oc create namespace minio
 oc create namespace observatorium
 
+#FIXME: make this a secret.yaml
+oc -n observatorium create secret generic tls-certs \
+    --from-file certs/server.pem \
+    --from-file certs/server.key \
+    --from-file certs/ca.pem
+
 oc apply -f environments/dev/manifests/
 oc expose svc/observatorium-xyz-observatorium-api-gateway -n observatorium
 
 observatoriu_svc_ip=`oc get svc/observatorium-xyz-observatorium-api-gateway -n observatorium \
     | tr -s " " | tail -1 | cut -d" " -f3`
 
+#FIXME: need to add clien cert and key to tlsConfig section - from secrets.
+#       As for today prometheus operator don't make use of this secret according
+#       to https://github.com/coreos/prometheus-operator/issues/3118
 echo "
 apiVersion: v1
 kind: ConfigMap
@@ -19,7 +28,7 @@ data:
   config.yaml: |
     prometheusK8s:
       remoteWrite:
-        - url: "http://$observatoriu_svc_ip:8080/api/metrics/v1/write"
+        - url: "https://$observatoriu_svc_ip:8080/api/metrics/v1/write"
           writeRelabelConfigs:
           - sourceLabels: [__name__]
             replacement: seal18_OS_cluster # this is the name of the cluster
@@ -27,6 +36,27 @@ data:
           tlsConfig:
             insecureSkipVerify: true
 " | oc apply -f -
+
+#echo "
+#apiVersion: v1
+#kind: ConfigMap
+#metadata:
+#  name: cluster-monitoring-config
+#  namespace: openshift-monitoring
+#data:
+#  config.yaml: |
+#    prometheusK8s:
+#      remoteWrite:
+#        - url: "https://$observatoriu_svc_ip:8080/api/metrics/v1/write"
+#          writeRelabelConfigs:
+#          - sourceLabels: [__name__]
+#            replacement: seal18_OS_cluster # this is the name of the cluster
+#            targetLabel: cluster
+#          tlsConfig:
+#            cert_file: client.crt
+#            key_file: client.key
+#            insecureSkipVerify: true
+#" | oc apply -f -
 
 oc scale --replicas=1 statefulset --all -n openshift-monitoring; \
     oc scale --replicas=1 deployment --all -n openshift-monitoring
